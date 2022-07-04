dad1a7750f3b29db583c17fe796088a4beecd252
rajkumar <razzatioe@gmail.com>
2022-07-04 08:36:11 +0545

    myreactapp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Resturent from './component/basic/Resturent'
+
+
+const App = () => {
+  return (
+    <>
+    <Resturent/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/component/basic/MenuApi.js
@@ -0,0 +1,76 @@
+const Menu =[
+    {
+        id : 1,
+        image: "images/maggi.jpg",
+        name: "Maggi",
+        category: "Breakfast",
+        price: 105,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+    {
+        id : 2,
+        image: "images/chola.jpg",
+        name: "Chola",
+        category: "Breakfast",
+        price: 125,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+    {
+        id : 3,
+        image: "images/allupakoida.jpg",
+        name: "Allupakoida",
+        category: "Dinner",
+        price: 100,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+    {
+        id : 4,
+        image: "images/rajmarice.jpg",
+        name: "Rajmarice",
+        category: "Lunch",
+        price: 120,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+    {
+        id : 5,
+        image: "images/samosa.jpg",
+        name: "samosa",
+        category: "Breakfast",
+        price: 75,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+    {
+        id : 6,
+        image: "images/samosa.jpg",
+        name: "samosa",
+        category: "Breakfast",
+        price: 75,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+    {
+        id : 7,
+        image: "images/samosa.jpg",
+        name: "samosa",
+        category: "Breakfast",
+        price: 75,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+    {
+        id : 8,
+        image: "images/samosa.jpg",
+        name: "samosa",
+        category: "Lunch",
+        price: 75,
+        description: "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+
+    },
+]
+
+export default Menu;
